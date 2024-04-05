0d1fbbdbe30b42261d50036b12e0ab5b6d11d6af
Aiden Foo <aidenfoo154@gmail.com>
2024-04-05 15:09:41 -0700

    adding to EventDTO

--- Models/EventModel.cs
+++ Models/EventModel.cs
@@ -8,8 +8,10 @@ namespace CourtMonitorBackend.Models.DTO
     public class EventModel
     {
         public string Title {get; set;}
+        public int Date {get; set;}
         public int StartTime {get; set;}
         public int EndTime {get; set;}
+        public string Color { get; set; }
 
         public EventModel()
         {
